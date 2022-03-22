567ca54d6a3fc2ebdbcdf8821795acf79f0883b6
Alejandro Mejia A <alejomejia1@gmail.com>
2022-03-22 16:15:57 -0500

    Version inicial de flask-datta-able con migrations en base de datos MySql (Mariadb) remota

--- apps/config.py
+++ apps/config.py
@@ -39,6 +39,16 @@ class ProductionConfig(Config):
 
 class DebugConfig(Config):
     DEBUG = True
+    
+    # PostgreSQL database
+    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
+        config('DB_ENGINE', default='postgresql'),
+        config('DB_USERNAME', default='appseed'),
+        config('DB_PASS', default='pass'),
+        config('DB_HOST', default='localhost'),
+        config('DB_PORT', default=5432),
+        config('DB_NAME', default='appseed-flask')
+    )
 
 
 # Load all possible configurations
